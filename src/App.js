279d7a3e8316feff2015feaf0eb33cdf17359f3f
suriming <87427848+suriming@users.noreply.github.com>
2022-12-08 19:42:45 +0900

    fix: mobile layout (#3)

--- src/App.js
+++ src/App.js
@@ -9,16 +9,44 @@ import CollectInput from './pages/CollectInput';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box minW="1024px">
+      <Box boxSizing="border-box">
         <Box
+          // minH="100vh"
+          // w="35.2rem"
+          // minW="600px"
+          // // m="0 auto"
+          // maxW="422px"
+          // // minW="450px"
+          // m={['0 auto', '0 0 0 calc(50vw - 1px)']}
+          // m={[
+          //   '0 auto', // 0-30em
+          //   '0 auto', // 30em-48em
+          //   '0 0 0 calc(50vw - 1px) auto', // 48em-62em
+          //   '0 0 0 calc(50vw - 1px) auto', // 62em+
+          // ]}
+          m={{
+            base: '0 auto', // 0-30em
+            md: '0 auto', // 30em-48em
+            lg: '0 0 0 calc(50vw - 1px)', // 48em-62em
+            xl: '0 0 0 calc(50vw - 1px)', // 62em+
+          }}
+          // // ml="730.5px"
+          // overflow="hidden"
+          // // boxSizing="border-box"
+          // boxShadow="lg"
+          // rounded="md"
           minH="100vh"
-          w="100%"
-          maxW="422px"
-          minW="450px"
-          m="0 0 0 calc(50vw - 1px)"
-          ml="730.5px"
+          // w={['22.2rem', '24.2rem', '100vw', '100vw']}
+          // w="20rem"
+          w={{
+            base: '24em', // 0-30em
+            md: '22rem', // 30em-48em
+            lg: '450px', // 48em-62em
+            xl: '450px', // 62em+
+          }}
+          minW="22rem"
+          // m="0 auto"
           overflow="hidden"
-          boxSizing="border-box"
           boxShadow="lg"
           rounded="md"
         >
