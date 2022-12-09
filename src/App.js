ece7e89e62c7c0ae94cdf98575357784e076c9ac
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2022-12-09 14:46:10 +0900

    [Chore] 필요없는 주석 제거 및 설명 주석 추가

--- src/App.js
+++ src/App.js
@@ -11,31 +11,10 @@ function App() {
     <ChakraProvider theme={theme}>
       <Box boxSizing="border-box">
         <Box
-          // minH="100vh"
-          // w="35.2rem"
-          // minW="600px"
-          // // m="0 auto"
-          // maxW="422px"
-          // // minW="450px"
-          // m={['0 auto', '0 0 0 calc(50vw - 1px)']}
-          // m={[
-          //   '0 auto', // 0-30em
-          //   '0 auto', // 30em-48em
-          //   '0 0 0 calc(50vw - 1px) auto', // 48em-62em
-          //   '0 0 0 calc(50vw - 1px) auto', // 62em+
-          // ]}
-          m={'0 auto'}
-          // // ml="730.5px"
-          // overflow="hidden"
-          // // boxSizing="border-box"
-          // boxShadow="lg"
-          // rounded="md"
+          m={'0 auto'} // 어플리케이션 영역을 중앙에 위치시킴
           minH="100vh"
-          // w={['22.2rem', '24.2rem', '100vw', '100vw']}
-          // w="20rem"
-          w="100vw"
-          maxW="48rem"
-          // m="0 auto"
+          w="100vw" // 어플리케이션 영역 너비를 뷰포트 너비로 설정
+          maxW="48rem" // 어플리케이션 영역 너비가 48rem(768px)을 넘지 않도록 설정
           overflow="hidden"
           boxShadow="lg"
           rounded="md"
